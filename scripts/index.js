a2221c8120ae962c4cf65435960663cbbfaa91f0
Danil Gorelkin <gorelkindanya@gmail.com>
2023-11-12 16:36:45 +0300

    v1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
-
+const cardList = document.querySelector('.places__list');
 // @todo: DOM узлы
 
 // @todo: Функция создания карточки
+const cardCreate = (cardName, cardImage, cardDelete) => {
+	const cardTemplate = document.querySelector('#card-template').content;
+	const cardItem = cardTemplate.querySelector('.card').cloneNode(true);
+	const deleteButton = cardItem.querySelector('.card__delete-button');
+
+	const cardPicture = cardItem.querySelector('.card__image');
+	cardPicture.src = cardImage;
+	cardPicture.alt = cardName;
+	cardItem.querySelector('.card__title').textContent = cardName;
+
+	cardList.append(cardItem);
+
+	deleteButton.addEventListener('click', cardDelete);
+}
 
 // @todo: Функция удаления карточки
+const cardDelete = (event) => {
+	event.currentTarget.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((cardItem) => {
+	cardCreate(cardItem.name, cardItem.link, cardDelete);
+});
